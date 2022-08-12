497b83dca1ff5d34960a70a9f2014f4dc3bb28ff
chaslinux <chaslinux@gmail.com>
2022-08-12 12:11:03 -0400

    Add the initial script to the repository.

---
+++ macbook-air-emc2925.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This script is designed to get the webcam working on Macbook Air A1466 with an EMC # of 2925
+# It may not work for A1466 Macbook Air laptops with a different EMC # as these are made different
+# years with different hardware
+#
+### Important:  Run this Script as a normal user
+#
+# by Charles McColm, chaslinux@gmail.com, August 12, 2022
+
+### Install Firmware ###
+
+sudo apt install git curl cpio
+mkdir -p ~/Code
+cd ~/Code
+git clone https://github.com/patjak/facetimehd-firmware.git
+cd facetimehd-firmware
+make
+sudo make install
+
+### Install webcam ###
+kernel=$(uname -r)
+kernel="linux-headers-$kernel"
+
+sudo apt -y install $kernel git kmod libssl-dev checkinstall
+cd ~/Code
+git clone https://github.com/patjak/bcwc_pcie.git
+cd bcwc_pcie
+make
+sudo checkinstall
+cd ~/Code/bcwc_pcie/
+sudo dpkg -i bcwc-pcie_20220812-1_amd64.deb
+sudo depmod
+sudo modprobe facetimehd
+
